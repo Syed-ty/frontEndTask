600e2cbaac0205628b2cc4a8e179709b9551bddd
TY_Syedhassan <syed.h@testyantra.in>
2023-07-13 12:47:59 +0530

    changes

--- src/app/dashboard/requirements/requirements.component.ts
+++ src/app/dashboard/requirements/requirements.component.ts
@@ -40,22 +40,22 @@ export class RequirementsComponent implements OnInit {
 
 
 
-  showLoadingIndicator:Boolean = false
+  showLoadingIndicator:Boolean = false;
 onSubmit(){
-  this.showLoadingIndicator = true
+  this.showLoadingIndicator = true;
   this.service.addApi(this.ScarpForm.value).subscribe((res)=>{
     if (!res.err) {
-      this.showLoadingIndicator=false
+      this.showLoadingIndicator=false;
       this.toast.success(res.message);
       localStorage.setItem('WebsiteDetails',JSON.stringify(res.response));
       this.ScarpForm.reset();
-      this.router.navigateByUrl('/website/screen2')
+      this.router.navigateByUrl('/website/screen2');
     } else {
       this.toast.error(res.message);
     }
   }, err => {
     if (err.status) {
-      this.showLoadingIndicator = false
+      this.showLoadingIndicator = false;
       this.toast.error(err.error.message);
     } else {
       this.showLoadingIndicator= false;
